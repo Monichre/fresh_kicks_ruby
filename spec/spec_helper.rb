6b8869fe4e33e21cd3a989658c712e9ff925e01c
Liam Ellis <liamhellis@gmail.com>
2016-07-22 08:45:50 -0700

    basic file structure up, database next

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+ENV['RACK_ENV'] = 'test'
+
+require("bundler/setup")
+Bundler.require(:default, :test)
+
+#Attempting to implement 'Shoulda-matcher' per the docs -->
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+  end
+end
+
+RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+set(:root, Dir.pwd())
+
+require('capybara/rspec')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require('./app')
