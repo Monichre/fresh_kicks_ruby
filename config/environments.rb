44b31e58a51a8e2b9869fa35ab7857410e443a07
Liam Ellis <liamhellis@gmail.com>
2016-08-09 18:01:21 -0500

    Procfile

--- config/environments.rb
+++
@@ -1,9 +0,0 @@
-configure :production, :development do
-  db = URI.parse(ENV['RACK_ENV'] || 'fresh_kicks_development')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => 'postgresql',
-    :database => 'fresh_kicks_development'
-
-  )
-end
